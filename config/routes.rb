95a381d0c88ee5fa39a9f7dad1126dbc019dd609
Esther Leytush <eleytush@gmail.com>
2016-09-15 09:00:34 -0500

    redrawing routes with add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-Rails.application.routes.draw do
-  namespace :main do
+RailsDribbbleOauth::Engine.add_routes do
     get '/users/auth/dribbble_request',     to: 'main#make_oauth_request', as: "user_dribbble_oauth_request"
     get '/users/auth/dribbble_callback',    to: 'main#passthru',           as: "user_dribbble_callback"
     get '/users/auth/dribbble_information', to: 'users#create',            as: "user_create_from_dribbble"
-  end
 end
+
+RailsDribbbleOauth::Engine.draw_routes
--- test/rails/dribbble/oauth_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Rails::Dribbble::Oauth::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Rails::Dribbble::Oauth
-  end
-end
